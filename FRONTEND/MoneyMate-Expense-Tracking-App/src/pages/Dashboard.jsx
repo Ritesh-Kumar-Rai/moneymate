e2178ea994543f63aebfb68e35be02fc16d20e96
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-07-19 23:52:56 +0530

    tried to add a modal and failed to achieve desired output will see tomorrow

--- FRONTEND/MoneyMate-Expense-Tracking-App/src/pages/Dashboard.jsx
+++ FRONTEND/MoneyMate-Expense-Tracking-App/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AddTransactionModal from '../components/ui/addTransactionModal';
 
 const Dashboard = () => {
     return (
         <div className='w-full bg-slate-100 dark:bg-gray-900 h-full min-h-screen p-5'>
             <h1 className='text-5xl font-bold'>Lorem ipsum dolor sit amet.</h1>
             {/* <button className='bg-indigo-600 text-white dark:bg-indigo-500 rounded-2xl px-3 hover:bg-indigo-500'>click me</button> */}
+            <AddTransactionModal />
         </div>
     )
 };
