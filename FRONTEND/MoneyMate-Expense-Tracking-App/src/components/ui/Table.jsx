be59e22a4c716a060cbca281cea63c40fe796a6a
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-07-25 00:14:15 +0530

    some changes made to Table.jsx like in caption element added `text-sm` class and for swap currency i replaced it from span element to button tag with type='button'

--- FRONTEND/MoneyMate-Expense-Tracking-App/src/components/ui/Table.jsx
+++ FRONTEND/MoneyMate-Expense-Tracking-App/src/components/ui/Table.jsx
@@ -6,7 +6,7 @@ const Table = () => {
         <div className="mt-10 w-full">
             <h2>Recent Transactions</h2>
             <table className="mt-5 w-full table-auto border-collapse border border-gray-400 bg-slate-200 dark:bg-gray-800 rounded overflow-hidden outline-1 outline-dashed outline-slate-400 shadow-2xl">
-                <caption className="caption-bottom mt-2">Recent 10 transactions of both income and expenses</caption>
+                <caption className="caption-bottom mt-3 text-sm">Recent 10 transactions of both income and expenses</caption>
                 <thead className="border-b-2 border-slate-400">
                     <tr>
                         <th className="border border-gray-600 px-3 py-2">Date</th>
@@ -24,7 +24,7 @@ const Table = () => {
                         <td className="border border-gray-600 px-3 py-2">Utilies (Bank Bill)</td>
                         <td className="border border-gray-600 px-3 py-2 text-center">$7843</td>
                         <td className="border border-gray-600 px-3 py-2 text-center relative">
-                            <span className="absolute left-[-7%] rounded-2xl bg-indigo-800 hover:bg-indigo-500 active:scale-90 text-white w-[50px] text-center cursor-pointer transition-scale-bg duration-200 will-change-transform select-none" title="swap currency"><CgSwap className="text-2xl m-auto" /> </span>
+                            <button type="button" className="absolute left-[-7%] rounded-2xl bg-indigo-800 hover:bg-indigo-500 active:scale-90 text-white w-[50px] text-center cursor-pointer transition-scale-bg duration-200 will-change-transform select-none" title="swap currency"><CgSwap className="text-2xl m-auto" /> </button>
                             $890373
                         </td>
                         <td className="border border-gray-600 px-3 py-2 text-center"><IoMdMore className="m-auto cursor-pointer text-2xl hover:bg-gray-500 rounded" /></td>
@@ -35,7 +35,7 @@ const Table = () => {
                         <td className="border border-gray-600 px-3 py-2">Utilies (Bank Bill)</td>
                         <td className="border border-gray-600 px-3 py-2 text-center">$7843</td>
                         <td className="border border-gray-600 px-3 py-2 text-center relative">
-                            <span className="absolute left-[-7%] rounded-2xl bg-indigo-800 hover:bg-indigo-500 active:scale-90 text-white w-[50px] text-center cursor-pointer transition-scale-bg duration-200 will-change-transform select-none" title="swap currency"><CgSwap className="text-2xl m-auto" /> </span>
+                            <button type="button" className="absolute left-[-7%] rounded-2xl bg-indigo-800 hover:bg-indigo-500 active:scale-90 text-white w-[50px] text-center cursor-pointer transition-scale-bg duration-200 will-change-transform select-none" title="swap currency"><CgSwap className="text-2xl m-auto" /> </button>
                             $890373
                         </td>
                         <td className="border border-gray-600 px-3 py-2 text-center"><IoMdMore className="m-auto cursor-pointer text-2xl hover:bg-gray-500 rounded" /></td>
@@ -46,7 +46,7 @@ const Table = () => {
                         <td className="border border-gray-600 px-3 py-2">Utilies (Bank Bill)</td>
                         <td className="border border-gray-600 px-3 py-2 text-center">$7843</td>
                         <td className="border border-gray-600 px-3 py-2 text-center relative">
-                            <span className="absolute left-[-7%] rounded-2xl bg-indigo-800 hover:bg-indigo-500 active:scale-90 text-white w-[50px] text-center cursor-pointer transition-scale-bg duration-200 will-change-transform select-none" title="swap currency"><CgSwap className="text-2xl m-auto" /> </span>
+                            <button type="button" className="absolute left-[-7%] rounded-2xl bg-indigo-800 hover:bg-indigo-500 active:scale-90 text-white w-[50px] text-center cursor-pointer transition-scale-bg duration-200 will-change-transform select-none" title="swap currency"><CgSwap className="text-2xl m-auto" /> </button>
                             $890373
                         </td>
                         <td className="border border-gray-600 px-3 py-2 text-center"><IoMdMore className="m-auto cursor-pointer text-2xl hover:bg-gray-500 rounded" /></td>
