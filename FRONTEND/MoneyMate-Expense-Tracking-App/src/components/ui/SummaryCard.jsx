28a1ec8791fcf6255c3da80ac2e403b71a181da7
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-07-30 21:52:21 +0530

    completed almost ExpensePage.jsx rest will be add/modify in future based on requirement... and In SummaryCard.jsx added another parameter for isMoney=true/false type which takes care whether currency symbol will display or not... because for normal value or string no need to display currency symbol before value

--- FRONTEND/MoneyMate-Expense-Tracking-App/src/components/ui/SummaryCard.jsx
+++ FRONTEND/MoneyMate-Expense-Tracking-App/src/components/ui/SummaryCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SummaryCard = ({ icon, custom_css_style = '', bg_color = 'bg-gray-800', label = '', value_amount = 0 }) => {
+const SummaryCard = ({ icon, custom_css_style = '', bg_color = 'bg-gray-800', label = '', isMoneyType = true, value_amount = 0 }) => {
     //  card 
     return (
         <div className={`total-summary-card flex gap-2 p-3 rounded-2xl shadow-2xl outline-2 outline-slate-400 hover:outline-blue-600 duration-200 transition-outline-color ${custom_css_style}`}>
@@ -10,7 +10,7 @@ const SummaryCard = ({ icon, custom_css_style = '', bg_color = 'bg-gray-800', la
             <div className='min-w-50 font-bold px-2 py-1'>
                 <dl>
                     <dt class="text-gray-500 text-xl mb-0.5">{label}</dt>
-                    <dd class="font-bold text-2xl">${value_amount}</dd>
+                    <dd class="font-bold text-2xl">{isMoneyType && '$'}{value_amount}</dd>
                 </dl>
             </div>
         </div>
--- FRONTEND/MoneyMate-Expense-Tracking-App/src/pages/ExpensePage.jsx
+++ FRONTEND/MoneyMate-Expense-Tracking-App/src/pages/ExpensePage.jsx
@@ -5,6 +5,7 @@ import SummaryCard from "../components/ui/SummaryCard";
 import GraphChart from "../components/charts/GraphChart";
 import { useState } from "react";
 import CustomBarChart from "../components/charts/CustomBarChart";
+import Table from "../components/ui/Table";
 
 const ExpensePage = () => {
 
@@ -14,8 +15,8 @@ const ExpensePage = () => {
         <section id="expense-page" className="w-full min-h-screen dark:bg-gray-900 p-5">
             <div className="summary-total-container w-full p-10 flex justify-between items-center flex-wrap gap-5 shadow-2xl dark:bg-gray-800 rounded-2xl">
                 <SummaryCard icon={<GiPayMoney />} bg_color='bg-red-700' label={"Total Expenses"} value_amount={2788} />
-                <SummaryCard icon={<TbCategory2 />} bg_color='bg-amber-700' label={"Highest Category"} value_amount={3788} />
-                <SummaryCard icon={<SiDatabricks />} bg_color='bg-sky-700' label={"Total Entries"} value_amount={788} />
+                <SummaryCard icon={<TbCategory2 />} bg_color='bg-amber-700' label={"Highest Category"} isMoneyType={false} value_amount="Electronics" />
+                <SummaryCard icon={<SiDatabricks />} bg_color='bg-sky-700' label={"Total Entries"} isMoneyType={false} value_amount={10} />
             </div>
 
             <div className='w-full mt-10 flex items-center justify-between'>
@@ -32,40 +33,58 @@ const ExpensePage = () => {
                     <option value="bar-chart">Bar Chart</option>
                 </select>
             </div>
+            {/* chart section */}
             <div className="mt-5 w-full">
                 <div className="">
                     <select name="" id="" className="bg-gray-50 border-1 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option value="all-categories" selected>All Categories</option>
-                        <option value="food">Food & Dining</option>
-                        <option value="groceries">Groceries</option>
-                        <option value="restaurant">Restaurants</option>
-                        <option value="cafe">Cafes & Coffee Shops</option>
-                        <option value="transportation">Transportation</option>
-                        <option value="fuel">Fuel & Gas</option>
-                        <option value="public_transport">Public Transport</option>
-                        <option value="shopping">Shopping</option>
-                        <option value="clothing">Clothing & Apparel</option>
-                        <option value="electronics">Electronics</option>
-                        <option value="health">Health & Medical</option>
-                        <option value="medication">Medication & Pharmacy</option>
-                        <option value="rent">Rent & Housing</option>
-                        <option value="utilities">Utilities (Electricity, Water, Gas)</option>
-                        <option value="insurance">Insurance</option>
-                        <option value="education">Education & Tuition</option>
-                        <option value="entertainment">Entertainment & Leisure</option>
-                        <option value="subscriptions">Subscriptions (Streaming, Magazines)</option>
-                        <option value="travel">Travel & Vacation</option>
-                        <option value="personal_care">Personal Care & Toiletries</option>
-                        <option value="gifts">Gifts & Donations</option>
-                        <option value="kids">Kids & Parenting</option>
-                        <option value="pets">Pets & Pet Care</option>
-                        <option value="others">Others</option>
+                        <option value="" selected>Pick a Range of Trend</option>
+                        <option value="3week">Last 3 week</option>
+                        <option value="1month">Last 1 month</option>
+                        <option value="3month">Last 3 month</option>
+                        <option value="6month">Last 6 month</option>
+                        <option value="1year">Last 1 year</option>
+                        <option value="3year">Last 3 year</option>
+                        <option value="6year">Last 5 year</option>
+                        <option value="all-time">All Time</option>
                     </select>
                 </div>
                 <div className="flex items-center place-content-center">
                     {(chart_type === "bar-chart") ? <CustomBarChart /> : <GraphChart type={chart_type} />}
                 </div>
             </div>
+            <hr className="my-5 border-2 border-gray-300 dark:border-slate-600 rounded-xs" />
+            {/* table filter section */}
+            <div className="mt-10 flex items-center place-content-end">
+                <select name="" id="" className="bg-gray-50 border-1 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="all-categories" selected>All Categories</option>
+                    <option value="food">Food & Dining</option>
+                    <option value="groceries">Groceries</option>
+                    <option value="restaurant">Restaurants</option>
+                    <option value="cafe">Cafes & Coffee Shops</option>
+                    <option value="transportation">Transportation</option>
+                    <option value="fuel">Fuel & Gas</option>
+                    <option value="public_transport">Public Transport</option>
+                    <option value="shopping">Shopping</option>
+                    <option value="clothing">Clothing & Apparel</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="health">Health & Medical</option>
+                    <option value="medication">Medication & Pharmacy</option>
+                    <option value="rent">Rent & Housing</option>
+                    <option value="utilities">Utilities (Electricity, Water, Gas)</option>
+                    <option value="insurance">Insurance</option>
+                    <option value="education">Education & Tuition</option>
+                    <option value="entertainment">Entertainment & Leisure</option>
+                    <option value="subscriptions">Subscriptions (Streaming, Magazines)</option>
+                    <option value="travel">Travel & Vacation</option>
+                    <option value="personal_care">Personal Care & Toiletries</option>
+                    <option value="gifts">Gifts & Donations</option>
+                    <option value="kids">Kids & Parenting</option>
+                    <option value="pets">Pets & Pet Care</option>
+                    <option value="others">Others</option>
+                </select>
+            </div>
+            {/* table section */}
+            <Table no_of_rows={10} label="All Time Expenses" caption="The list of all expenses made by you" custom_style_class="mt-5" transactions_to_show="expense" />
         </section>
     );
 };
